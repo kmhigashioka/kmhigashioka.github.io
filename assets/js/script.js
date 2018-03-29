13ce290672831b41fd0414464672a1e2a7cc9f68
Kazuhito Higashioka <kazu@bizbox.ph>
2018-03-30 02:32:53 +0800

    Relayout pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,90 @@
 $(document).ready(function () {
   $(function() {
-    $.scrollify({
-      section : ".full-height",
-      sectionName : "section-name"
-    });
     
-    ['articles', 'projects', 'contact'].forEach(function (t) {
-      $("a[href*='" + t + "']").click(function() {
-        $.scrollify.move("#" + t);
+    function smoothScrollingTo(target){
+      $('html,body').animate({scrollTop:$(target).offset().top}, 500);
+    }
+
+    $('a[href*=\\#]').on('click', function(event){     
+        event.preventDefault();
+        smoothScrollingTo(this.hash);
+    });
+
+    $(document).ready(function(){
+      smoothScrollingTo(location.hash);
+    });
+
+    $('#inquiry-form').submit(function (event) {
+      event.preventDefault();
+
+      var notyOpt = {
+        theme: 'sunset',
+        inTimeout: 5000,
+        outTimeout: 1000
+      };
+
+      var info = new Noty({
+        theme: notyOpt.theme,
+        text: 'Sending query.',
+        type: 'information',
+        timeout: notyOpt.inTimeout
+      });
+
+      info.show();
+
+      $.ajax({
+        method: 'POST',
+        data: JSON.stringify({
+          "personalizations": [
+            {
+              "to": [
+                {
+                  "email": "john@example.com"
+                }
+              ],
+              "subject": "Hello, World!"
+            }
+          ],
+          "from": {
+            "email": "from_address@example.com"
+          },
+          "content": [
+            {
+              "type": "text/plain",
+              "value": "Hello, World!"
+            }
+          ]
+        }
+        ),
+        dataType: 'json',
+        processData: false,
+        url: 'https://api.sendgrid.com/v3/mail/send',
+        headers: {
+          'Authorization': 'bearer ',
+          'Content-Type': 'application/json'
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          info.setTimeout(notyOpt.outTimeout);
+
+          new Noty({
+            theme: notyOpt.theme,
+            text: 'Sending of query encountered a problem. Please try again later.',
+            type: 'error',
+            timeout: notyOpt.inTimeout
+          }).show();
+        },
+        success: function (jqXHR, textStatus, errorThrown) {
+          info.setTimeout(notyOpt.outTimeout);
+
+          new Noty({
+            theme: notyOpt.theme,
+            text: 'Query successfully sent from <email here>.',
+            type: 'success',
+            timeout: notyOpt.inTimeout
+          }).show();
+        }
       });
     });
 
-    $('.projects .masonry-container .content')
-      .on('mouseover', function () {
-        $(this).addClass('fadeIn');
-        $(this).removeClass('fadeOut');
-      })
-      .mouseleave(function () {
-        $(this).removeClass('fadeIn');
-        $(this).addClass('fadeOut');
-      });
-
-    $(this).bind('scroll', onChangeBackgroundColor);
-    onChangeBackgroundColor();
-
-    function onChangeBackgroundColor() {
-      if ($('.welcome h1').visible(true)) {
-        $('body').removeClass();
-        $('body').addClass('gray-bgcolor');
-      }
-      if ($('.articles .article-container').visible(true)) {
-        $('body').removeClass();
-        $('body').addClass('pink-bgcolor');
-      }
-      if ($('.projects .project-container').visible(true)) {
-        $('body').removeClass();
-        $('body').addClass('gray-bgcolor');
-      }
-      if ($('.contact h1').visible(true)) {
-        $('body').removeClass();
-        $('body').addClass('dark-gray-bgcolor');
-      }
-    }
   });
 });
