103b06bdcd983b14493932c18757d592631a09db
Kazuhito Higashioka <kmhigashioka@gmail.com>
2019-04-09 09:10:01 +0800

    Remove logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React, { Fragment } from 'react';
 import Headroom from 'react-headroom';
-import { Flex, Image } from 'rebass';
+import { Flex } from 'rebass';
 import styled from 'styled-components';
 import { SectionLinks } from 'react-scroll-section';
 import Fade from 'react-reveal/Fade';
 import RouteLink from './RouteLink';
-import Logo from './Logo/Portfolio.svg';
 
 const capitalize = s => s && s[0].toUpperCase() + s.slice(1);
 
@@ -16,6 +15,10 @@ const HeaderContainer = styled(Headroom)`
 
   position: absolute;
   width: 100%;
+`;
+
+const SectionLinksContainer = styled(SectionLinks)`
+  min-height: 82px;
 `;
 
 const formatLinks = allLinks =>
@@ -44,21 +47,10 @@ const Header = () => (
         alignItems="center"
         p={3}
       >
-        <SectionLinks>
+        <SectionLinksContainer>
           {({ allLinks }) => {
-            const { home, links } = formatLinks(allLinks);
+            const { links } = formatLinks(allLinks);
 
-            const homeLink = home && (
-              <Image
-                src={Logo}
-                width="50px"
-                alt="Portfolio Logo"
-                onClick={home.onClick}
-                style={{
-                  cursor: 'pointer',
-                }}
-              />
-            );
             const navLinks = links.map(({ name, value }) => (
               <RouteLink
                 key={name}
@@ -71,12 +63,12 @@ const Header = () => (
 
             return (
               <Fragment>
-                {homeLink}
+                <span />
                 <Flex mr={[0, 3, 5]}>{navLinks}</Flex>
               </Fragment>
             );
           }}
-        </SectionLinks>
+        </SectionLinksContainer>
       </Flex>
     </Fade>
   </HeaderContainer>
